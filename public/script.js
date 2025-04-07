c615778fb12194e4af25d379dbf3303ba0290891
amaury <amauryav@ucm.es>
2025-04-08 01:27:12 +0200

    test

--- public/script.js
+++ public/script.js
@@ -1,13 +1,20 @@
 const socket = io('https://test-9p0r.onrender.com'); 
 
-document.getElementById("create-lobby-button").addEventListener(e, () => {
-  createLobby();
+document.getElementById("create-lobby-button").addEventListener("click", () => {
+    console.log(`Conectado con id: ${socket.id}`);
 });
 
+socket.on("connect", () => {
+    console.log(`Conectado con id: ${socket.id}`);
+});
+
+
 async function createLobby(name) {
+    console.log('Intentando crear un lobby');
   socket.emit('create-lobby', { playerName: name});
 
   socket.on('lobby-created', (lobby) => {
+    console.log(`Se ha creado un lobby con codigo: ${lobby.lobbyId}`)
     window.location.href = `lobby.html?lobby=${lobby.lobbyId}`;
   })
 }
--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const io = require("socket.io")(server, {
 
 app.use(cors());
 
-io.on("connection", (socket) => {
+io.on("connection", socket => {
   console.log("A new user has connected: ", socket.id);
 });
 
