2e1b1ee2af7ca977818267b8d1d652d41c6089f2
amaury <amauryav@ucm.es>
2025-04-08 01:36:32 +0200

    simple

--- server/index.js
+++ server/index.js
@@ -4,14 +4,9 @@ const io = require("socket.io")(http, {
   cors: { origin: "*" },
 });
 
-io.on("connection", socket => {
+io.on("connection", (socket) => {
   console.log("A new user has connected: ", socket.id);
+  socket.on("message", (message) => {
+    console.log(message);
+  });
 });
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
-
-
-
