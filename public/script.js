b993b9ed5e308f069edb3971f9edf200f7b74400
amaury <amauryav@ucm.es>
2025-04-08 02:52:54 +0200

    front

--- public/script.js
+++ public/script.js
@@ -1,15 +1,15 @@
 const socket = io("https://test-9p0r.onrender.com");
 
-document.getElementById('create-lobby-button').addEventListener("click", () => {
-  createLobby("Amaury");
+document.getElementById("create-lobby-button").addEventListener("click", () => {
+  createLobby(document.getElementById("player-name-input").value);
 });
 
-document.getElementById('join-lobby-button').addEventListener("click", () => {
-    joinLobby(
-        document.getElementById('lobby-code-input').value,
-        document.getElementById('player-name-input').value
-    )
-})
+document.getElementById("join-lobby-button").addEventListener("click", () => {
+  joinLobby(
+    document.getElementById("lobby-code-input").value,
+    document.getElementById("player-name-input").value
+  );
+});
 
 socket.on("connect", () => {
   console.log(`Conectado con id: ${socket.id}`);
@@ -17,23 +17,28 @@ socket.on("connect", () => {
 
 async function createLobby(_playerName) {
   console.log("Intentando crear un lobby");
-  socket.emit('create-lobby', { playerName: _playerName }, lobbyCode => {
+  socket.emit("create-lobby", { playerName: _playerName }, (lobbyCode) => {
     goToLobby(lobbyCode);
   });
 }
 
 function goToLobby(_lobbyCode) {
-    if(_lobbyCode === null) 
-        console.error('Error al unirse al lobby');
-    window.location.href = `lobby.html?lobby=${_lobbyCode}`;
+  if (_lobbyCode === null) console.error("Error al unirse al lobby");
+  window.location.href = `lobby.html?lobby=${_lobbyCode}`;
 }
 
 async function joinLobby(_lobbyCode, _playerName) {
-  console.log(`Intentando unirse al lobby ${_lobbyCode} con nombre ${_playerName}`);
-  socket.emit('join-lobby', {
-    lobbyCode: _lobbyCode,
-    playerName: _playerName
-  }, lobbyCode => {
-    goToLobby(lobbyCode);
-  })
+  console.log(
+    `Intentando unirse al lobby ${_lobbyCode} con nombre ${_playerName}`
+  );
+  socket.emit(
+    "join-lobby",
+    {
+      lobbyCode: _lobbyCode,
+      playerName: _playerName,
+    },
+    (lobbyCode) => {
+      goToLobby(lobbyCode);
+    }
+  );
 }
